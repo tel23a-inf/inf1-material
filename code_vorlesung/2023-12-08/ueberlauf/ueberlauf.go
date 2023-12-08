fa2e0e8d7eb94f127824b42a9ac75273dd8b689f
Reiner Hüchting <dev@reinerhuechting.de>
2023-12-08 14:50:52 +0100

    Beispiele zu Überläufen.

---
+++ code_vorlesung/2023-12-08/ueberlauf/ueberlauf.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var b byte = 255
+
+	fmt.Println(b)
+	fmt.Println(b + 1)
+
+	var u int8
+
+	for i := 0; i < 270; i++ {
+		fmt.Println(u)
+		u++
+	}
+
+}

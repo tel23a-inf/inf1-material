9f237f1f92ce991e1ef5c1176a143d2e4f09d08f
Reiner Hüchting <dev@reinerhuechting.de>
2023-12-22 12:08:17 +0100

    Weitere Beispiele aus Vorlesung hinzugefügt.

--- code_vorlesung/2023-12-22/listen/listen.go
+++ code_vorlesung/2023-12-22/listen/listen.go
@@ -1,6 +1,9 @@
 package listen
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func IntLists() {
 	// Erzeugen einer Liste mit Elementen vom Typ int.
@@ -30,4 +33,93 @@ func StringLists() {
 	// Anhängen von Elementen an die Liste.
 	list2 = append(list2, "Hallo", "Welt")
 	fmt.Println(list2)
+
+	// Erzeugen einer Liste mit Werten.
+	list3 := []string{"Foo", "Bar"}
+	fmt.Println(list3)
+}
+
+// Liefert eine Liste mit dem zehnfachen der Zahlen von 1 bis n.
+func NumberList(n int) []int {
+	result := []int{} // Leere Liste.
+	for i := 1; i <= n; i++ {
+		result = append(result, 10*i)
+	}
+	return result
+}
+
+// Erwartet eine Zahl n und liefert eine Liste mit den Ziffern von n.
+func Digits(n int) []int {
+	result := []int{}
+
+	// Schleife, die die einzelnen Ziffern aus der Zahl
+	// berechnet und in result schreibt.
+	for n != 0 {
+		result = append(result, n%10) // z.B. 321 % 10 == 1
+		n = n / 10
+	}
+
+	// Liste umdrehen (generiert von VsCode).
+	// for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
+	// 	result[i], result[j] = result[j], result[i]
+	// }
+	result = Revert(result)
+
+	return result
+}
+
+// Erwartet eine Liste und liefert eine Liste mit den Einträgen in umgekehrter Reihenfolge.
+func Revert(list []int) []int {
+	result := []int{}
+
+	for i := len(list) - 1; i >= 0; i-- {
+		result = append(result, list[i])
+	}
+
+	return result
+}
+
+func PrintRow(row []string) {
+	fmt.Print("|")
+	for i := 0; i < len(row); i++ {
+		fmt.Print(" ")
+		fmt.Print(row[i])
+		fmt.Print(" |")
+	}
+	fmt.Println()
+}
+
+func PrintRow2(row []string) {
+	fmt.Print("|")
+	for _, el := range row {
+		fmt.Print(" ")
+		fmt.Print(el)
+		fmt.Print(" |")
+	}
+	fmt.Println()
+}
+
+func PrintBoard(board [][]string) {
+	// "+---" * len(board[0])
+	border := Border(len(board[0]))
+	fmt.Println(border)
+	for _, row := range board {
+		PrintRow(row)
+		fmt.Println(border)
+	}
+}
+
+func Border(width int) string {
+	return fmt.Sprintf("%s+", strings.Repeat("+---", width))
+
+	// Alternative ohne Printf
+	// return strings.Repeat("+---", width) + "+"
+
+	// "händische" Alternative
+	// border := ""
+	// for i := 0; i < width; i++ {
+	// 	border += "+---"
+	// }
+	// border += "+"
+	// return border
 }
--- code_vorlesung/2023-12-22/listen/listen_test.go
+++ code_vorlesung/2023-12-22/listen/listen_test.go
@@ -1,5 +1,7 @@
 package listen
 
+import "fmt"
+
 func ExampleIntLists() {
 	IntLists()
 
@@ -18,4 +20,70 @@ func ExampleStringLists() {
 	// Output:
 	// []
 	// [Hallo Welt]
+	// [Foo Bar]
+}
+
+func ExampleNumberList() {
+	list1 := NumberList(15)
+	fmt.Println(list1)
+
+	second := list1[1]
+	fmt.Println(second)
+
+	// Output:
+}
+
+func ExampleDigits() {
+	fmt.Println(Digits(42))
+	fmt.Println(Digits(321))
+	fmt.Println(Digits(5))
+	fmt.Println(Digits(0))
+
+	// Output:
+	// [4 2]
+	// [3 2 1]
+	// [5]
+	// []
+}
+
+func ExamplePrintRow() {
+	row1 := []string{"1", "2", "3"}
+	PrintRow(row1)
+	PrintRow2(row1)
+
+	row2 := []string{" ", "X", " ", "O", "O"}
+	PrintRow(row2)
+	PrintRow2(row2)
+
+	// Output:
+	// | 1 | 2 | 3 |
+	// | 1 | 2 | 3 |
+	// |   | X |   | O | O |
+	// |   | X |   | O | O |
+}
+
+func ExamplePrintBoard() {
+	// Ein Spielfeld ist eine Liste,
+	// deren Elemente Listen von Strings sind.
+	// Jede solche innere Liste ist eine Zeile auf
+	// dem Spielfeld.
+	b1 := [][]string{
+		{" ", "X", " ", "X"},
+		{" ", "X", "O", "O"},
+		{"O", " ", " ", "X"},
+	}
+
+	PrintBoard(b1)
+
+	// Wie muss eine Funktionssignatur
+	// plus Gerüst für PrintBoard aussehen?
+
+	// Output:
+	// +---+---+---+---+
+	// |   | X |   | X |
+	// +---+---+---+---+
+	// |   | X | O | O |
+	// +---+---+---+---+
+	// | O |   |   | X |
+	// +---+---+---+---+
 }
